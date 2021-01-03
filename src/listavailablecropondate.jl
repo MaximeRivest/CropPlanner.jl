5839288be59b16efe6a61dd89e18f05743635b56
Maxime Rivest <mrive052@gmail.com>
2021-01-03 11:02:54 -0500

    update

--- src/listavailablecropondate.jl
+++ src/listavailablecropondate.jl
@@ -1,17 +1,22 @@
 function listavailablecropondate(date)
     #transform date to weeks from beginning of year
+    wdate = Week(Date(date)).value
 
     #check table of range of harvest dates
-        #vegetable, first_harvest(weeks), last_harvest(weeks)
-        # broccoli,                   32,                  40
+    possible_harvest_weeks = CSV.read("data/possible_harvest_weeks.csv", DataFrame)
+    subdf = @where(possible_harvest_weeks, :week .== wdate)
 
     #check price table
+    price_table = CSV.read("data/crop plan - jm_yields_and_sales.csv", DataFrame)
+    cropinfo = @pipe leftjoin(subdf, price_table, on = :vegetable) |>
+        @select(_, :vegetable, :week, :date, :our_yield_kg_or_unit, :desired_selling_price, 
+        :unit_of_selling_price, :recommended_quantity, :roughly_equivalent_to, :price_of_recommended_quantity, :predicted_revenu_per_our_bed)
 
     #check standard quantity table
 
     #calculate price per standard quantity
 
     #format table
-
-    return  # vegetable, price, unit, price/m2, standard_quantity, price_per_standard_quantity
+    println(sum(skipmissing(cropinfo[:, :price_of_recommended_quantity])))
+    return  cropinfo# vegetable, price, unit, price/m2, standard_quantity, price_per_standard_quantity
 end
